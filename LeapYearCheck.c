19c4a5dc425093fa9f8211cf1fad1be0d1ace6e9
Prakhar Indoria <prakharindoria@gmail.com>
2020-09-25 23:51:14 +0530

    Update LeapYearCheck.c

--- LeapYearCheck.c
+++ LeapYearCheck.c
@@ -1,21 +1,21 @@
 main(){
-    int a;
-
+int year;
 printf("Enter Year to check: ");
-scanf("%d",&a);
-if(a%100==0){
-    if(a%400==0)
+scanf("%d",&year);
+    
+if(a%100==0){ //Check if the year is century year or not.  Like => 1900,2000,2100,2200
+    if(a%400==0) //Check if it is multiple of 400 like 2000,1600,1200  =>These are leap years.
         printf("Leap Year");
-    else
-        printf("Not a Leap Year");
+    else   
+        printf("Not a Leap Year"); //If the year is Divisible by 100 but not by 400, It is not a leap year.
 
 }
-else
+else  //If a year is not century year
 {
-    if(a%4==0)
+    if(a%4==0)  //Check if year is divisible by 4
        printf("Leap Year");
-    else
-        printf("Not a Leap Year");
+    else   
+        printf("Not a Leap Year");  //Not divisible by 4.
 }
 }
 
