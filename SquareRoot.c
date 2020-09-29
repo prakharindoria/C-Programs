61266e1df3e0e2ca0d5a7143472272d4c06241ce
Prakhar Indoria <prakharindoria@gmail.com>
2020-09-29 22:45:03 +0530

    Delete SquareRoot.c

--- SquareRoot.c
+++
@@ -1,8 +0,0 @@
-//Program needs improvement as it does  not account for the logic of %400 ,example 1700,1900,2100 are not leap years.
-main(){
-int a;
-printf("Enter a Year:");
-scanf("%d",&a);
-if(a%4==0)
-    printf("LEAP YEAR");
-}
